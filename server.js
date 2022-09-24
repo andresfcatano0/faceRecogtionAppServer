2570c63db6922c188c57d76f2d879e632120bdd2
Andres Catano <andresfcatano0@gmail.com>
2022-09-24 09:26:05 -0400

    Add database connection

--- server.js
+++ server.js
@@ -10,11 +10,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-defined-24038',
-    // port : 3306,
-    user : 'andrescatano',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
