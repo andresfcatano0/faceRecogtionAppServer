8d7a2535b03869c4222d25b86845b922c101ec3d
Andres Catano <andresfcatano0@gmail.com>
2022-09-24 09:17:20 -0400

    Update database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'postgresql-defined-24038',
     // port : 3306,
     user : 'andrescatano',
     password : '',
